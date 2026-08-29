0bb149864b2987507daf61dc200c01882176f4a6
ParametricFlow <parametricflow@gmail.com>
2026-08-29 11:03:46 -0600

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-// ======================================
-// CONFIGURACIÓN GLOBAL DEL SITIO
-// ======================================
-
-window.PF_CONFIG = Object.freeze({
-    FORMSPREE_ENDPOINT: "https://formspree.io/f/mrewqldl",
-    CONTACT_EMAIL: "parametricflow@gmail.com",
-    MOBILE_BREAKPOINT_PX: 640
-});
